2b20d83b806a84d8f6af507711055d3a89588402
6031 <dqas1004t@naver.com>
2024-12-24 13:49:36 +0900

    #1 - data rest 설정

--- src/main/java/com/fastcampus/projectBoard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectBoard/repository/ArticleRepository.java
@@ -2,7 +2,9 @@ package com.fastcampus.projectBoard.repository;
 
 import com.fastcampus.projectBoard.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 
 }
