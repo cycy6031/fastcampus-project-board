9a1a2508fb25d1962976b7250de848521a138119
leebomi <124218687+cycy6031@users.noreply.github.com>
2024-12-30 10:50:44 +0900

    # - 회원 계정 도메인 구현 및 erd 반영

--- src/main/java/com/fastcampus/projectBoard/domain/ArticleComment.java
+++ src/main/java/com/fastcampus/projectBoard/domain/ArticleComment.java
@@ -2,23 +2,16 @@ package com.fastcampus.projectBoard.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
 import java.util.Objects;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @ToString
