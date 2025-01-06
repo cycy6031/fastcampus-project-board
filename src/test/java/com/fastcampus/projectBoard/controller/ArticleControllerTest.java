d20e94a6f09504b80ea471516337d17e0609c44a
6031 <dqas1004t@naver.com>
2025-01-06 16:47:38 +0900

    #16 - 게시판 컨트롤러 구현 게시판 기본 조회 화면을 위한 컨트롤러 구현하고 데이터 전달 완료 게시판 페이지 기본 정렬 속성 부여: 10개 1페이지, 시간 내림차순

--- src/test/java/com/fastcampus/projectBoard/controller/ArticleControllerTest.java
+++ src/test/java/com/fastcampus/projectBoard/controller/ArticleControllerTest.java
@@ -1,6 +1,10 @@
 package com.fastcampus.projectBoard.controller;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -8,13 +12,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import com.fastcampus.projectBoard.config.SecurityConfig;
+import com.fastcampus.projectBoard.dto.ArticleWithCommentsDto;
+import com.fastcampus.projectBoard.dto.UserAccountDto;
+import com.fastcampus.projectBoard.service.ArticleService;
+import java.time.LocalDateTime;
+import java.util.Set;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @DisplayName("View 컨트롤러 - 게시글")
@@ -24,6 +36,8 @@ class ArticleControllerTest {
 
     private final MockMvc mvc;
 
+    @MockitoBean private ArticleService articleService;
+
     public ArticleControllerTest(@Autowired MockMvc mvc) {
         this.mvc = mvc;
     }
@@ -32,6 +46,8 @@ class ArticleControllerTest {
     @Test
     public void givenNoting_whenRequestingArticlesView_thenReturnsArticlesView() throws Exception{
         // Given
+        given(articleService.searchArticles(eq(null), eq(null), any(Pageable.class))).willReturn(
+            Page.empty());
 
         // When & Then
         mvc.perform(get("/articles"))
@@ -39,20 +55,24 @@ class ArticleControllerTest {
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/index"))
             .andExpect(model().attributeExists("articles"));
+        then(articleService).should().searchArticles(eq(null), eq(null), any(Pageable.class));
     }
 
     @DisplayName("[view][GET] 게시글 상세 페이지 - 정상 호출")
     @Test
     public void givenNoting_whenRequestingArticlesView_thenReturnsArticleView() throws Exception{
         // Given
+        Long articleId = 1L;
+        given(articleService.getArticle(articleId)).willReturn(createArticleWithCommentsDto());
 
         // When & Then
-        mvc.perform(get("/articles/1"))
+        mvc.perform(get("/articles/" + articleId))
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/detail"))
             .andExpect(model().attributeExists("article"))
             .andExpect(model().attributeExists("articleComments"));
+        then(articleService).should().getArticle(articleId);
     }
 
     @Disabled("진행 중")
@@ -79,5 +99,36 @@ class ArticleControllerTest {
             .andExpect(status().isOk())
             .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
             .andExpect(view().name("articles/search-hashtag"));
+    }
+
+
+    private ArticleWithCommentsDto createArticleWithCommentsDto() {
+        return ArticleWithCommentsDto.of(
+            1L,
+            createUserAccountDto(),
+            Set.of(),
+            "title",
+            "content",
+            "#java",
+            LocalDateTime.now(),
+            "6031",
+            LocalDateTime.now(),
+            "6031"
+        );
+    }
+
+    private UserAccountDto createUserAccountDto() {
+        return UserAccountDto.of(
+            1L,
+            "6031",
+            "password",
+            "6031@email.com",
+            "6031",
+            "this is memo",
+            LocalDateTime.now(),
+            "6031",
+            LocalDateTime.now(),
+            "6031"
+        );
     }
 }
