ead202748cadc51409556d766d96e7fad2eb2157
cycy6031 <dqas1004t@naver.com>
2025-01-13 23:49:47 +0900

    # - ing

--- src/main/java/com/fastcampus/projectBoard/controller/ArticleController.java
+++ src/main/java/com/fastcampus/projectBoard/controller/ArticleController.java
@@ -1,6 +1,9 @@
 package com.fastcampus.projectBoard.controller;
 
+import com.fastcampus.projectBoard.domain.constant.FormStatus;
 import com.fastcampus.projectBoard.domain.constant.SearchType;
+import com.fastcampus.projectBoard.dto.UserAccountDto;
+import com.fastcampus.projectBoard.request.ArticleRequest;
 import com.fastcampus.projectBoard.response.ArticleResponse;
 import com.fastcampus.projectBoard.response.ArticleWithCommentResponse;
 import com.fastcampus.projectBoard.service.ArticleService;
@@ -15,6 +18,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,7 +48,7 @@ public class ArticleController {
 
     @GetMapping("/{articleId}")
     public String article(@PathVariable Long articleId, ModelMap map) {
-        ArticleWithCommentResponse article = ArticleWithCommentResponse.from(articleService.getArticle(articleId));
+        ArticleWithCommentResponse article = ArticleWithCommentResponse.from(articleService.getArticleWithComment(articleId));
 
         map.addAttribute("article", article);
         map.addAttribute("articleComments", article.articleCommentResponses());
@@ -53,7 +57,7 @@ public class ArticleController {
     }
 
     @GetMapping("/search-hashtag")
-    public String searchHashtag(
+    public String searchArticleHashtag(
         @RequestParam(required = false, name = "searchValue") String searchValue,
         @PageableDefault(size = 10, sort = "createdAt", direction = Direction.DESC) Pageable pageable,
         ModelMap map
@@ -68,5 +72,44 @@ public class ArticleController {
         map.addAttribute("searchType", SearchType.HASHTAG);
 
         return "articles/search-hashtag";
+    }
+
+    @GetMapping("/form")
+    public String articleForm(ModelMap map){
+        map.addAttribute("formStatus", FormStatus.CREATE);
+        return "article/form";
+    }
+
+    @PostMapping("/form")
+    public String postNewArticle(ArticleRequest articleRequest){
+        articleService.saveArticle(articleRequest.toDto(UserAccountDto.of(
+            "uno", "asdf1234", "uno@mail.com", "Uno", "I am Uno.", null, null, null, null
+        )));
+
+        return "redirect:/articles";
+    }
+
+    @GetMapping("/{articleId}/form")
+    public String updateArticleForm(@PathVariable Long articleId, ModelMap map){
+        ArticleResponse article = ArticleResponse.from(articleService.getArticle(articleId));
+
+        map.addAttribute("article", article);
+        map.addAttribute("formStatus", FormStatus.UPDATE);
+
+        return "articles/form";
+    }
+
+    @PostMapping("/{articleId}/form")
+    public String updateArticle(@PathVariable Long articleId, ArticleRequest articleRequest){
+        articleService.updateArticle(articleId, articleRequest.toDto(UserAccountDto.of(
+            "uno", "asdf1234", "uno@mail.com", "Uno", "I am Uno.", null, null, null, null
+        )));
+    }
+
+    @PostMapping("/{articleId}/delete")
+    public String deleteArticle(@PathVariable Long articleId){
+        articleService.deleteArticle(articleId);
+
+        return "redirect:/articles";
     }
 }
